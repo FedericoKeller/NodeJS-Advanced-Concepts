d16a34d106b83be7bb8fd741ed2be9b886b3695f
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-12-07 13:40:35 -0300

    NodeJS: Advanced Concepts - Enhancing Node Performance - Clustering in action

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express');
+const cluster = require('cluster');
 
-const app = express();
+// Is the file being executed in master mode?
+if(cluster.isMaster) {
+    // Cause index.js to be executed *again* but 
+    // in child mode
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+} else {
+    // I m a child, I'm going to act like a server
+    // and do nothing else
+    const express = require('express');
 
-const doWork = (duration) => {
-    const start = Date.now();
+    const app = express();
 
-    while(Date.now() - start < duration) {
-        
+    const doWork = (duration) => {
+        const start = Date.now();
+
+        while(Date.now() - start < duration) {}
     }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    })
+
+    app.get('/fast', (req, res) => {
+        res.send('Hi was fast!');
+    })
+
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    res.send('Hi there');
-})
-
-app.listen(3000);
