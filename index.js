f1f744b78ae230d2e67d37c61c5f9fa0c0d52fde
Kelprim <43957146+Kelprim@users.noreply.github.com>
2021-12-07 14:47:39 -0300

    NodeJS: Advanced Concepts - Enhancing Node Performance - Benchmarking Server Performance

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 // Is the file being executed in master mode?
 if(cluster.isMaster) {
     // Cause index.js to be executed *again* but 
     // in child mode
     cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+
 } else {
     // I m a child, I'm going to act like a server
     // and do nothing else
@@ -15,15 +17,11 @@ if(cluster.isMaster) {
 
     const app = express();
 
-    const doWork = (duration) => {
-        const start = Date.now();
-
-        while(Date.now() - start < duration) {}
-    }
 
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there');
+        crypto.pbkdf2('a' , 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there');
+        })
     })
 
     app.get('/fast', (req, res) => {
